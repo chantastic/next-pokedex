de7cf50ff4d8705cf6078b32834f1bb14b901be5
Michael Chan <mijoch@gmail.com>
2022-12-17 17:59:41 -0800

    add layout to pokemon show page

---
+++ app/[name]/layout.jsx
@@ -0,0 +1,26 @@
+export default async function PokemonLayout({ children }) {
+  let res = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
+  let data = await res.json();
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "10rem 1fr",
+        padding: "2rem",
+      }}
+    >
+      <aside>
+        <ul>
+          {data.results.map((pokemon) => (
+            <li key={pokemon.name}>
+              <a href={`/${pokemon.name}`}>{pokemon.name}</a>
+            </li>
+          ))}
+        </ul>
+      </aside>
+
+      {children}
+    </div>
+  );
+}
