5bd71ff95d28c0530f6f25ce4636045c306c43d7
Michael Chan <mijoch@gmail.com>
2022-12-20 12:34:01 -0800

    move sidebare list from layout to page

--- app/pokemon/[name]/page.jsx
+++ app/pokemon/[name]/page.jsx
@@ -1,25 +1,74 @@
 import Image from "next/image";
+import Link from "next/link";
 
-async function fetchPokemonByName(name) {
-  let res = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${name}`
-  );
+async function fetchPokemon({ id = "", params = {} }) {
+  let searchParams = new URLSearchParams(params).toString();
+  let endpoint = new URL(
+    `https://pokeapi.co/api/v2/pokemon/${id}?${searchParams}`
+  ).toString();
 
+  let res = await fetch(endpoint);
   return await res.json();
 }
 
-export default async function Home({ params }) {
-  let pokemon = await fetchPokemonByName(params.name);
+export default async function Home({ params, searchParams }) {
+  let pokemon = await fetchPokemon({ id: params.name });
+  let pokemonList = await fetchPokemon({
+    params: searchParams,
+  });
 
   return (
-    <main>
-      <h1>{pokemon.name}</h1>
-      <Image
-        src={pokemon.sprites.front_default}
-        alt={`${pokemon.name} front`}
-        width="96"
-        height="96"
-      />
-    </main>
+    <>
+      <aside>
+        <ul>
+          {pokemonList.results.map((pokemon) => (
+            <li key={pokemon.name}>
+              <Link
+                href={getPokemonUrl(pokemon.name, searchParams)}
+              >
+                {pokemon.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Link
+          href={getPokemonUrl(pokemon.name, {
+            limit: Number(searchParams.limit || 20) + 20,
+          })}
+        >
+          Load more
+        </Link>
+      </aside>
+
+      <main>
+        <div
+          style={{
+            position: "-webkit-sticky",
+            position: "sticky",
+            top: 0,
+          }}
+        >
+          <h1>{pokemon.name}</h1>
+          <Image
+            src={pokemon.sprites.front_default}
+            alt={`${pokemon.name} front`}
+            width="96"
+            height="96"
+          />
+        </div>
+      </main>
+    </>
   );
+}
+
+function getPokemonUrl(pokemonName = "bulbasaur", params = {}) {
+  let path = `/pokemon/${pokemonName}`;
+  let listSearchParams = "";
+  if (params.limit) {
+    listSearchParams = new URLSearchParams([
+      ["limit", params.limit],
+    ]).toString();
+  }
+
+  return `${path}?${listSearchParams}`;
 }
--- app/pokemon/layout.jsx
+++ app/pokemon/layout.jsx
@@ -1,9 +1,4 @@
-import Link from "next/link";
-
 export default async function PokemonLayout({ children }) {
-  let res = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
-  let data = await res.json();
-
   return (
     <div
       style={{
@@ -12,18 +7,6 @@ export default async function PokemonLayout({ children }) {
         padding: "2rem",
       }}
     >
-      <aside>
-        <ul>
-          {data.results.map((pokemon) => (
-            <li key={pokemon.name}>
-              <Link href={`/pokemon/${pokemon.name}`}>
-                {pokemon.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </aside>
-
       {children}
     </div>
   );
